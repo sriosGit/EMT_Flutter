2cb0c5c1a262fd123aabba0c5798afec2f2dd5d3
Sergio Rios <srios0u@gmail.com>
2020-11-20 05:42:55 -0500

    CHECKPOINT - update to do

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
 import 'package:EMT/screens/loginScreen.dart';
+import 'package:call_log/call_log.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:workmanager/workmanager.dart';
+
+var callLogs;
+
+void callbackDispatcher() {
+  Workmanager.executeTask((task, inputData) async {
+    print(
+        "Native called background task: $task"); //simpleTask will be emitted here.
+    // CallLog.query(
+    //  dateFrom: from,
+    //  dateTo: to,
+    //).then((callLogEntries) {
+    print(callLogs);
+    // });
+    return Future.value(true);
+  });
+}
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager.initialize(
+      callbackDispatcher, // The top level function, aka callbackDispatcher
+      isInDebugMode:
+          true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+      );
   runApp(MyApp());
 }
 
@@ -14,14 +38,44 @@ class _MyApp extends State<MyApp> {
   // This widget is the root of your application.
   bool _initialized = false;
   bool _error = false;
+
+  Map<String, dynamic> _convertCallLogsToMap(entry) {
+    Map<String, dynamic> map = {};
+    map['formattedNumber'] = entry.formattedNumber;
+    map['cachedMatchedNumber'] = entry.cachedMatchedNumber;
+    map['number'] = entry.number;
+    map['name'] = entry.name;
+    map['callType'] = entry.callType;
+    map['cachedMatchedNumber'] =
+        DateTime.fromMillisecondsSinceEpoch(entry.timestamp);
+    map['duration'] = entry.duration;
+    print(map);
+    return map;
+  }
+
   @override
   void initState() {
-    initializeFlutterFire();
+    var now = DateTime.now();
+    int from = now.subtract(Duration(days: 7)).millisecondsSinceEpoch;
+    int to = now.millisecondsSinceEpoch;
+
+    CallLog.query(
+      dateFrom: from,
+      dateTo: to,
+    ).then((callLogEntries) {
+      callLogs = callLogEntries.map((e) => _convertCallLogsToMap(e)).toList();
+      Workmanager.registerPeriodicTask(
+        "23",
+        "notSoSimplePeriodicTask23",
+        // When no frequency is provided the default 15 minutes is set.
+        // Minimum frequency is 15 min. Android will automatically change your frequency to 15 min if you have configured a lower frequency.
+      ); //Android only (see below)
+    });
     super.initState();
   }
 
   void initializeFlutterFire() async {
-    print("HELLLOOOOOOOOOOOOOOO");
+    print("Hello World, I'm EMT");
     try {
       // Wait for Firebase to initialize and set `_initialized` state to true
       await Firebase.initializeApp();
